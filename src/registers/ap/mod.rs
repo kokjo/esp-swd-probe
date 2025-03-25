5a5153bff9cf562fae2ef6c54ec2e95b214057d2
Jonas Rudloff <jonas.t.rudloff@gmail.com>
2025-03-25 21:13:47 +0100

    Make IDR use make_register!

--- src/registers/ap/mod.rs
+++ src/registers/ap/mod.rs
@@ -1,3 +1,5 @@
+use crate::make_register;
+
 pub trait APRegister {
     const ADDRESS: u8;
 }
@@ -13,13 +15,24 @@ pub enum APType {
     Unknown(u8),
 }
 
-impl From<u8> for APType {
-    fn from(value: u8) -> Self {
+impl From<u32> for APType {
+    fn from(value: u32) -> Self {
         match value {
             0 => APType::Jtag,
             1 => APType::AHB,
             2 => APType::APB,
-            x => APType::Unknown(x),
+            x => APType::Unknown(x as u8),
+        }
+    }
+}
+
+impl From<APType> for u32 {
+    fn from(value: APType) -> Self {
+        match value {
+            APType::Jtag => 0,
+            APType::AHB => 1,
+            APType::APB => 2,
+            APType::Unknown(x) => x as u32,
         }
     }
 }
@@ -27,58 +40,48 @@ impl From<u8> for APType {
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum APClass {
     NoClass,
-    ComAP,
+    ComAp,
     MemAp,
     Unknown(u8),
 }
 
-impl From<u8> for APClass {
-    fn from(value: u8) -> Self {
+impl From<u32> for APClass {
+    fn from(value: u32) -> Self {
         match value {
             0b0000 => APClass::NoClass,
-            0b0001 => APClass::ComAP,
+            0b0001 => APClass::ComAp,
             0b1000 => APClass::MemAp,
-            x => APClass::Unknown(x),
+            x => APClass::Unknown(x as u8),
         }
     }
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Idr(pub u32);
+impl From<APClass> for u32 {
+    fn from(value: APClass) -> Self {
+        match value {
+            APClass::NoClass => 0b0000,
+            APClass::ComAp => 0b0001,
+            APClass::MemAp => 0b1000,
+            APClass::Unknown(x) => x as u32,
+        }
+    }
+}
+
+make_register!(Idr, {
+    (ap_type, 0, 4, APType),
+    (variant, 4, 4),
+    (class, 13, 4, APClass),
+    (revision, 28, 4),
+    (designer, 17, 11)
+});
 
 impl APRegister for Idr {
     const ADDRESS: u8 = 0xfc;
 }
 
-impl From<u32> for Idr {
-    fn from(value: u32) -> Self {
-        Self(value)
-    }
-}
-
 impl ReadRegister for Idr {}
 
 impl Idr {
-    pub fn ap_type(&self) -> APType {
-        ((self.0 & 0xf) as u8).into()
-    }
-
-    pub fn variant(&self) -> u8 {
-        ((self.0 >> 4) & 0xf) as u8
-    }
-
-    pub fn class(&self) -> APClass {
-        (((self.0 >> 13) & 0xf) as u8).into()
-    }
-
-    pub fn revision(&self) -> u8 {
-        ((self.0 >> 28) & 0xf) as u8
-    }
-
-    pub fn designer(&self) -> u32 {
-        (self.0 >> 17) & 0x7ff
-    }
-
     pub fn is_mem_ap(&self) -> bool {
         self.class() == APClass::MemAp
     }
@@ -88,17 +91,5 @@ impl Idr {
     }
 }
 
-impl core::fmt::Debug for Idr {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        f.debug_struct("Idr")
-            .field("raw", &self.0)
-            .field("ap_type()", &self.ap_type())
-            .field("class()", &self.class())
-            .field("variant()", &self.variant())
-            .field("revision()", &self.revision())
-            .field("designer()", &self.designer())
-            .finish()
-    }
-}
 
 pub mod memap;
