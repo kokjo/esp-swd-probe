794a4659744cc377c34d8a76f1d6a350ff1efae7
Jonas Rudloff <jonas.t.rudloff@gmail.com>
2025-03-25 21:14:22 +0100

    more bits in ctrlstat register

--- src/registers/dp/ctrlstat.rs
+++ src/registers/dp/ctrlstat.rs
@@ -8,7 +8,16 @@ make_register!(CtrlStat, {
     (cdbgpwrupack, 29, 1, bool),
     (cdbgpwrupreq, 28, 1, bool),
     (cdbgrstack, 27, 1, bool),
-    (cdbgrstreq, 26, 1, bool)
+    (cdbgrstreq, 26, 1, bool),
+    (trncnt, 12, 12),
+    (masklane, 8, 4),
+    (wdataerr, 7, 1, bool),
+    (readok, 6, 1, bool),
+    (stickyerr, 5, 1, bool),
+    (stickycmp, 4, 1, bool),
+    (trnmode, 2, 2, bool),
+    (stickyorun, 1, 1, bool),
+    (orundetect, 0, 1, bool)
 });
 
 impl DPRegister for CtrlStat {
